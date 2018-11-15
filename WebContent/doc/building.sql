1ee4c0d82a88ded4db99b97ea277a62bff10d6ed
肖老师PC <472036660@qq.com>
2018-11-15 20:07:47 +0800

    数据库

---
+++ WebContent/doc/building.sql
@@ -0,0 +1,2 @@
+INSERT INTO `building` VALUES ('1', '3栋', '1', '12', '大数据工程学院');
+INSERT INTO `building` VALUES ('2', '4栋', '1', '32', '外国语学院');
---
+++ WebContent/doc/dormitory.sql
@@ -0,0 +1,2 @@
+INSERT INTO `dormitory` VALUES ('1', '2', '3', 4, 5, 6, 7, 8);
+INSERT INTO `dormitory` VALUES ('2', '沙发上', '沙发', 4, 1, 4, 600, 50);
