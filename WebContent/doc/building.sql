a180bd8fd0c5584df515708a134b5af641aedf9a
肖老师PC <472036660@qq.com>
2018-11-15 21:00:31 +0800

    肖老师更新，大家都用阿里数据库

--- WebContent/doc/building.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `building` VALUES ('1', '3栋', '1', '12', '大数据工程学院');
-INSERT INTO `building` VALUES ('2', '4栋', '1', '32', '外国语学院');
--- WebContent/doc/dormitory.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `dormitory` VALUES ('1', '2', '3', 4, 5, 6, 7, 8);
-INSERT INTO `dormitory` VALUES ('2', '沙发上', '沙发', 4, 1, 4, 600, 50);
