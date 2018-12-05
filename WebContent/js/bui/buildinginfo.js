13f7124a4206d6e503761fc8fe4176f55d8f21ef
蒋霜 <1456882850@qq.com>
2018-12-05 19:44:00 +0800

    蒋霜

--- WebContent/js/bui/buildinginfo.js
+++ WebContent/js/bui/buildinginfo.js
@@ -1,13 +1,13 @@
-layui.config({
+layui.config({//框架的固定，配置的使用
 	base : "js/"
-}).use(['form','layer','jquery','laypage','table'],function(){
+}).use(['form','layer','jquery','laypage','table'],function(){//组件，使用组件完成功能：from:表单；
 	var form = layui.form,
 		layer = parent.layer === undefined ? layui.layer : parent.layer,
 		laypage = layui.laypage,
 		table = layui.table,
-		$ = layui.$;
+		$ = layui.$;//以上只是将所需要的文件拿出来，以便于后面使用。
 
-//==================一个table实例================================
+//==================一个table实例================================//table怎么设置
 	  table.render({
 	    elem: '#demo',//渲染对象
 	    height: 400,//表格高度
@@ -30,7 +30,7 @@ layui.config({
 		  reload : function() {
 			  var demoReload = $('#demoReload');
 							// 执行重载
-			  table.reload('testReload', {
+			  table.reload('testReload', {//reload重新加载
 				  page : {
 					  curr : 1// 重新从第 1 页开始
 					  },
@@ -42,21 +42,22 @@ layui.config({
 			  }
   };
 //绑定搜索事件
+  //将事件绑定在按钮上
   $('.layui-btn').on('click', function() {
 	  var type = $(this).data('type');
 	  active[type] ? active[type].call(this) : '';
 	  });
   
 //=============绑定【添加】事件============================
-	$(window).one("resize",function(){
+	$(window).one("resize",function(){//基于框架做的
 		$(".add_btn").click(function(){
 			var index = layui.layer.open({
 				title : "【添加信息】",
 				icon: 2,
 				type : 2,
 				area: ['800px', '600px'],
-				content : "openBuildingAdd",
-				success : function(layero, index){
+				content : "openBuildingAdd",//加载某一张页面
+				success : function(layero, index){//等同于js异步提交
 					setTimeout(function(){
 						layui.layer.tips('点击此处返回列表', '.layui-layer-setwin .layui-layer-close', {
 							tips: 3
@@ -70,6 +71,7 @@ layui.config({
   
 //=======================监听工具条====================================
 
+	//给编辑和删除增加动作，直接应用layui
 	table.on('tool(test)', function(obj){ //注：tool是工具条事件名，test是table原始容器的属性 lay-filter="对应的值"
 	  var data = obj.data; //获得当前行数据
 	  var layEvent = obj.event; //获得 lay-event 对应的值（也可以是表头的 event 参数对应的值）
--- WebContent/js/cms/cms_userAdd.js
+++ WebContent/js/cms/cms_userAdd.js
@@ -1,44 +1,13 @@
 var $;
 layui.config({
 	base : "js/"
-}).use(['form','layer','jquery','upload'],function(){
+}).use(['form','layer','jquery'],function(){
 	var form = layui.form,
 		layer = parent.layer === undefined ? layui.layer : parent.layer,
-		upload = layui.upload,
 		laypage = layui.laypage;
 		$ = layui.jquery;
-
-		 //普通图片上传
-		  var uploadInst = upload.render({
-		    elem: '#test1',
-		    url: '/file/uploadImg',
-		    //exts: 'pdf',//文件类型 pdf|doc|docx
-		    before: function(obj){
-		      //预读本地文件示例，不支持ie8
-		      obj.preview(function(index, file, result){
-		        $('#demo1').attr('src', result); //图片链接（base64）
-		      });
-		    }
-		    ,done: function(res){
-		      //如果上传失败
-		      if(res.code > 0){
-		        return layer.msg('上传失败');
-		      }
-		      //上传成功
-		      $("#img_url").val(res.url);
-		      console.log(res.url); 
-		    }
-		    ,error: function(){
-		      //演示失败状态，并实现重传
-		      var demoText = $('#demoText');
-		      demoText.html('<span style="color: #FF5722;">上传失败</span> <a class="layui-btn layui-btn-xs demo-reload">重试</a>');
-		      demoText.find('.demo-reload').on('click', function(){
-		        uploadInst.upload();
-		      });
-		    }
-		  });	
-		
- 	form.on("submit(addUser)",function(data){console.log(data.field);
+//===========================================		
+ 		form.on("submit(add)",function(data){
  		var index;
  		 $.ajax({//异步请求返回给后台
 	    	  url:'saveCms_User',
@@ -57,7 +26,7 @@ layui.config({
 			  	 	parent.location.reload();
 		    		
 	    	  },
-	    	  error:function(XMLHttpRequest, textCms_User, errorThrown){
+	    	  error:function(XMLHttpRequest, textStatus, errorThrown){
 	    		  top.layer.msg('保存失败！！！服务器有问题！！！！<br>请检测服务器是否启动？', {
 	    		        time: 20000, //20s后自动关闭
 	    		        btn: ['知道了']
@@ -66,5 +35,4 @@ layui.config({
 	      });
  		return false;
  	})
-	
-})
+ 	})
